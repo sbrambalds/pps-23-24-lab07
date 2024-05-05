e8df49a973e44182d1853252d93bf1f11a4cd8df
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-05-05 14:09:38 +0200

    End Solitaire implementation

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -1,5 +1,7 @@
 package ex3
 
+import ex3.Solitaire.valid
+
 import javax.print.attribute.standard.Destination
 import scala.collection
 import scala.util.Random
@@ -11,24 +13,22 @@ object Solitaire:
 
   private val width = 5
   private val height = 5
-  private val start: Cell = (width/2, height/2)
+  private val startPos: Cell = (width/2, height/2)
 
   given IterableFactory = LazyList(_)
 
   def placeMarks(movesLeft: Int = width*height)(using factory: IterableFactory): Iterable[Solution] = movesLeft match
-    case 0 => factory(List(start))
+    case 0 => factory(List(startPos))
     case _ =>
-      val positions = placeMarks(movesLeft - 1)
       for
-        pos <- positions
+        sol <- placeMarks(movesLeft - 1)
         x <- 0 to width
         y <- 0 to height
         cell = (x, y)
-        if valid(pos, cell)
-      yield
-        pos.++(List(cell))
+        if valid(sol, cell)
+      yield sol.++(List(cell))
 
-  def valid(solution: Iterable[Cell], dest: Cell): Boolean =  !solution.exists(p => p == dest) & correct(solution.last, dest)
+  def valid(solution: Iterable[Cell], dest: Cell): Boolean =  !solution.exists(p => p == dest) & correct(solution.head, dest)
 
   def correct(source: Cell, dest: Cell): Boolean =
     val x = (source._1 - dest._1).abs
@@ -36,12 +36,12 @@ object Solitaire:
     (x == 3 & y == 0) | (x == 0 & y == 3) | (x == 2 & y == 2)
 
   def render(sol: Seq[Cell]): String =
-    val reversed = sol
     val rows = for y <- 0 until height
         row = for x <- 0 until width
-        number = reversed.indexOf((x,y)) + 1
+        number = sol.indexOf((x,y)) + 1
         yield if number > 0 then "%-2d ".format(number) else "X  "
       yield row.mkString
     rows.mkString("\n")
 
-  @main def run (): Unit = placeMarks().zipWithIndex.foreach({solution => println("Solution " + solution._2); println(render(solution._1.toList))})
+  @main def run (): Unit =
+    placeMarks().zipWithIndex.foreach({solution => println("Solution " + solution._2 + "\n" + render(solution._1.toList))})
