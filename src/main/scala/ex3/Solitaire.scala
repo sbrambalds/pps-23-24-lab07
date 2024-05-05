30fac69c28c7f9709693c01991302ba6f8f848ae
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-05-05 12:58:42 +0200

    End Solitaire implementation

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -1,15 +1,47 @@
 package ex3
 
-object Solitaire extends App:
-  def render(solution: Seq[(Int, Int)], width: Int, height: Int): String =
-    val reversed = solution.reverse
-    val rows =
-      for y <- 0 until height
-          row = for x <- 0 until width
-          number = reversed.indexOf((x, y)) + 1
-          yield if number > 0 then "%-2d ".format(number) else "X  "
+import javax.print.attribute.standard.Destination
+import scala.collection
+import scala.util.Random
+
+object Solitaire:
+  type Cell = (Int, Int)
+  type Solution = Iterable[Cell]
+  type IterableFactory = Solution => Iterable[Solution]
+
+  private val width = 5
+  private val height = 5
+  private val start: Cell = (width/2, height/2)
+
+  given IterableFactory = LazyList(_)
+
+  def placeMarks(movesLeft: Int = width*height)(using factory: IterableFactory): Iterable[Solution] = movesLeft match
+    case 0 => factory(List(start))
+    case _ =>
+      val positions = placeMarks(movesLeft - 1)
+      for
+        pos <- positions
+        x <- 0 to width
+        y <- 0 to height
+        cell = (x, y)
+        if valid(pos, cell)
+      yield
+        pos.++(List(cell))
+
+  def valid(solution: Iterable[Cell], dest: Cell): Boolean =  !solution.exists(p => p == dest) & correct(solution.last, dest)
+
+  def correct(source: Cell, dest: Cell): Boolean =
+    val x = (source._1 - dest._1).abs
+    val y = (source._2 - dest._2).abs
+    (x == 3 & y == 0) | (x == 0 & y == 3) | (x == 2 & y == 2)
+
+  def render(sol: Seq[Cell]): String =
+    val reversed = sol
+    val rows = for y <- 0 until height
+        row = for x <- 0 until width
+        number = reversed.indexOf((x,y)) + 1
+        yield if number > 0 then "%-2d ".format(number) else "X  "
       yield row.mkString
     rows.mkString("\n")
 
-
-  println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+  @main def run (): Unit = placeMarks().zipWithIndex.foreach({solution => println("Solution " + solution._2); println(render(solution._1.toList))})
